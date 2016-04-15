3bf79c3ada446605897db744d07ac4b67424d8e1
xhamyd <xhamyd@users.noreply.github.com>
2016-04-15 17:47:12 -0400

    Built the basics of the walk function, only has the initial drawing

--- show_order.py
+++ show_order.py
@@ -1,6 +1,15 @@
+from Tkinter import *
+
 DANCER_FILE = 'S16 Assigned-Unassigned - Sheet1.csv'
 OUTSIDE_ORGS_FILE = '[DS] Outside Orgs Sign-Up S16 - Sheet1.csv'
 SHOW_ORDER_FILE = 'Show Order Format - S16.csv'
+
+def make_list(dic):
+    s = []
+    for act in dic:
+        for piece in dic[act]:
+            s.append(piece)
+    return s
 
 class Show_Order(object):
     
@@ -49,6 +58,14 @@ class Show_Order(object):
 
         self.ShowOrder[act_i_d2][dance2_i], self.ShowOrder[act_i_d1][dance1_i] = self.ShowOrder[act_i_d1][dance1_i], self.ShowOrder[act_i_d2][dance2_i]
         print "Switch between %s and %s complete!" % (dance1, dance2)
+
+    def no_conflict(self, piece):
+        result = []
+        for act in self.ShowOrder:
+            for piece_i in self.ShowOrder[act]:
+                if piece_i not in self.conflictMap[piece]:
+                    result.append(piece_i)
+        return result
 
 def conflict(dancers1, dancers2):
     for dancer in dancers1:
@@ -122,5 +139,144 @@ def master_run():
     else: print "\nVerdict: Review previous errors"
 
     return SO
+
+def walk(SO):
+
+    def points_to_pixels(pt): # techically (pt*4/3) but was making things too big on the page
+        return round(pt * 4 / 5) 
+
+    def get_maxCellWidth(pp_list):
+        maxCellWidth = 0
+        for piece in pp_list:
+            if maxCellWidth < len(piece): 
+                maxCellWidth = len(piece)
+        return maxCellWidth
+        
+    def make_possible_pieces(SO):
+        possible_pieces = []
+        for act in SO.ShowOrder:
+            for piece in SO.ShowOrder[act]:
+                possible_pieces.append(piece)
+        possible_pieces.sort()
+        possible_pieces.insert(0, "Possible Pieces")
+        return possible_pieces
+
+    def init():
+        data.possible_pieces = make_possible_pieces(SO)
+        maxCellWidth = get_maxCellWidth(data.possible_pieces)
+        # x is width, y is height
+        font_size = 8
+
+        data.cellWidth = points_to_pixels(maxCellWidth * font_size)
+        data.cellHeight_pp, data.cellHeight_final = 3*font_size, 4*font_size
+
+        total_cellHeight_pp = data.cellHeight_pp*(len(data.possible_pieces)+1) 
+        total_cellHeight_final = data.cellHeight_final*(max(len(SO.ShowOrder["ACT 1"]), len(SO.ShowOrder["ACT 2"])) + 1)
+
+        data.borderMargin, data.middleMargin = 5, 20
+        data.winWidth = 2*data.borderMargin + 4*data.cellWidth + 2*data.middleMargin
+        data.winHeight = 2*data.borderMargin + max(total_cellHeight_pp, total_cellHeight_final)
+
+        data.final_ShowOrder = []
+        data.functionButtons = ["RESET", "UNDO", ""]
+        maxFunCellWidth = get_maxCellWidth(data.functionButtons)
+        font_size_fun = 12
+        data.funCellWidth = points_to_pixels(maxFunCellWidth * font_size_fun * 1.5)
+        data.funCellHeight = 4 * font_size_fun
+
+    def mousePressed(event):
+        print (event.x, event.y)
+                                
+    def timerFired(): pass
+
+    def redrawAll(canvas):
+        # possible pieces
+        canvas.create_text(data.borderMargin + data.cellWidth / 2,
+                           data.borderMargin + data.cellHeight_pp,
+                           text=data.possible_pieces[0], font="Arial 12 bold")
+        for i in xrange(1, len(data.possible_pieces)):
+            canvas.create_text(data.borderMargin + data.cellWidth / 2, 
+                               data.borderMargin + (i+1) * data.cellHeight_pp,
+                               text=data.possible_pieces[i])
+            corners_pp = (data.borderMargin, 
+                          data.borderMargin + ((2*i+1) * data.cellHeight_pp) / 2,
+                          data.borderMargin + data.cellWidth,
+                          data.borderMargin + ((2*i+3) * data.cellHeight_pp) / 2) #x0, y0, x1, y1
+            canvas.create_rectangle(corners_pp)
+
+        # final ShowOrder (@TODO: Implement with new list)
+        canvas.create_text(data.borderMargin + 3 * data.cellWidth / 2 + data.middleMargin,
+                           data.borderMargin + data.cellHeight_final / 2,
+                           text="ACT 1", font="Arial 12 bold")
+        for i in xrange(1, len(SO.ShowOrder["ACT 1"])):
+            canvas.create_text(data.borderMargin + 3 * data.cellWidth / 2 + data.middleMargin,
+                               data.borderMargin + (data.cellHeight_final * (2*i + 1)) / 2,
+                               text=SO.ShowOrder["ACT 1"][i])
+            corners_final_1 = (data.borderMargin + data.cellWidth + data.middleMargin, 
+                               data.borderMargin + i * data.cellHeight_final,
+                               data.borderMargin + 2 * data.cellWidth + data.middleMargin,
+                               data.borderMargin + (i+1) * data.cellHeight_final) #x0, y0, x1, y1
+            canvas.create_rectangle(corners_final_1)
+
+        canvas.create_text(data.borderMargin + 5 * data.cellWidth / 2 + data.middleMargin,
+                           data.borderMargin + data.cellHeight_final / 2,
+                           text="ACT 2", font="Arial 12 bold")
+        for i in xrange(1, len(SO.ShowOrder["ACT 2"])):
+            canvas.create_text(data.borderMargin + 5 * data.cellWidth / 2 + data.middleMargin,
+                               data.borderMargin + (data.cellHeight_final * (2*i + 1)) / 2,
+                               text=SO.ShowOrder["ACT 2"][i])
+            corners_final_2 = (data.borderMargin + 2 * data.cellWidth + data.middleMargin, 
+                               data.borderMargin + i * data.cellHeight_final,
+                               data.borderMargin + 3 * data.cellWidth + data.middleMargin,
+                               data.borderMargin + (i+1) * data.cellHeight_final) #x0, y0, x1, y1
+            canvas.create_rectangle(corners_final_2)
+
+
+        # bottom-right three buttons
+        for i in xrange(len(data.functionButtons)):
+            canvas.create_text(data.winWidth - data.borderMargin - data.funCellWidth / 2,
+                               data.winHeight - data.borderMargin - ((2 * (len(data.functionButtons) - 1 - i) + 1) * data.funCellHeight) / 2,
+                               text=data.functionButtons[i], font="Arial 12")
+            corners_function = (data.winWidth - data.borderMargin - data.funCellWidth, 
+                                data.winHeight - data.borderMargin - (len(data.functionButtons) - 1 - i) * data.funCellHeight,
+                                data.winWidth - data.borderMargin,
+                                data.winHeight - data.borderMargin - (len(data.functionButtons) - i) * data.funCellHeight) #x0, y0, x1, y1
+            canvas.create_rectangle(corners_function)
+
+    def redrawAllWrapper(canvas):
+        canvas.delete(ALL)
+        redrawAll(canvas)
+        canvas.update()
+
+    def mousePressedWrapper(event, canvas):
+        mousePressed(event)
+        redrawAllWrapper(canvas)
+
+    def keyPressedWrapper(event, canvas):
+        keyPressed(event)
+        redrawAllWrapper(canvas)
+
+    def timerFiredWrapper(canvas):
+        timerFired()
+        redrawAllWrapper(canvas)
+        # pause, then call timerFired again
+        canvas.after(data.timerDelay, timerFiredWrapper, canvas)
+        
+    # Set up data and call init
+    class Struct(): pass
+    data = Struct()
+    data.timerDelay = 100 # milliseconds
+    init()
+    # create the root and the canvas
+    root = Tk()
+    root.wm_title("Walk Algorithm")
+    canvas = Canvas(root, width=data.winWidth, height=data.winHeight)
+    canvas.pack()
+    # set up events
+    root.bind("<Button-1>", lambda event: mousePressedWrapper(event, canvas))
+    timerFiredWrapper(canvas)
+    # and launch the app
+    root.mainloop() # blocks until window is closed
+    print("Walk done.")
 
 master_run()
